58d6ae949f91afbafc3ade741dc6876200bc97b0
Shaydesh <shaydeshner@protonmail.com>
2026-02-08 10:31:05 -0800

    service worker change

--- src/hooks/useServiceWorkerUpdate.ts
+++ src/hooks/useServiceWorkerUpdate.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export function useServiceWorkerUpdate() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/serviceWorker.js', {
+      navigator.serviceWorker.register('/serviceWorker.js?v=152', {
           updateViaCache: 'none'
         })
         .then((registration) => {
